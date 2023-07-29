bdf2c35e10aca4045fd5f620a7d9a18e55440015
SchollSaaS <v.tsarukyan@doslab.ru>
2023-07-29 18:47:46 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Главная</title>
+        <link rel="stylesheet" href="./assets/styles/style.css">
+    </head>
+    <body>
+        <?php require './moduls/header.php'?>
+        <div class="container">
+            <div class="item">
+                <img src="" alt="">
+                <h4 class="title_item">Название товара</h4>
+                <div class="rating">4.5⭐</div>
+                <span class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda cupiditate doloremque dolorum enim!</span>
+                <a href="#">Подробнее</a>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ moduls/header.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<header>
+    <a href="./">CloudShop</a>
+    <form action="" method="get">
+        <input type="text" placeholder="Поиск">
+        <input type="submit" value="Поиск">
+    </form>
+    <a href="#">П</a>
+</header>
